404953af8b0a3ba342bd9220705851ba9b3d802e
Zach Kangas <zkangas6@gmail.com>
2023-03-11 22:40:16 -0600

    final floors and verbump

--- RadarPlugin/UtilInfo.cs
+++ RadarPlugin/UtilInfo.cs
@@ -28,22 +28,13 @@ public static class UtilInfo
     {
         { 5832, 16f } //mimic 2
     };
-
-    /**
-     * What is this used for again?
-     * Oh yeah, thats right, it's not (currently)
-     */
-    public static Dictionary<uint, Vector4> HuntRecolors = new Dictionary<uint, Vector4>()
-    {
-        { 8131, new Vector4(1f, 1f, 1f, 1f) } // white - Hydatos Elemental 
-    };
-
+    
     public static HashSet<ushort> DeepDungeonMapIds = new()
     {
         561, 562, 563, 564, 565, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, // POTD
         770, 771, 772, 782, 773, 783, 774, 784, 775, 785, // HOH
         732, 763, 795, 827, // Eureka (Old) (in order)
-        1099, 1100, 1101, 1102, 1103, 1104, 1105, 1106, 1107, // Eureka Orthos in order
+        1099, 1100, 1101, 1102, 1103, 1104, 1105, 1106, 1107, 1108, // Eureka Orthos in order
     };
 
     /**
